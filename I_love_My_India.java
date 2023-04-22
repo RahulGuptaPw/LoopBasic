1ae96e7b62c8d1cf2782d4b48ab51cf0f691f39f
RahulGuptaPw <rahulgupta26022002@gmail.com>
2023-04-22 16:02:57 +0530

    first Commit

---
+++ I_love_My_India.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Loop  
+{
+public static void main(String args[])
+{
+
+int counter = 0;
+
+while (counter < 100)
+{
+
+System.out.println("I Love My India");
+
+counter++;
+
+
+}
+
+System.out.println("INDIA");
+
+}
+
+}
